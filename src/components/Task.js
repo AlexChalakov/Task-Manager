c23c71eb996755ea0b3ba8e7ed25745b11e104d8
Alexcheto <a.chalakov@lancaster.ac.uk>
2021-08-30 22:50:23 +0300

    init commit

---
+++ src/components/Task.js
@@ -0,0 +1,12 @@
+import { FaTimesCircle } from 'react-icons/fa'
+
+const Task = ({ task }) => {
+    return (
+        <div className = 'task'>
+            <h3> {task.text} <FaTimesCircle style={{ color: 'red', cursor: 'pointer' }} />  </h3>
+            <p> {task.day} </p>
+        </div>
+    )
+}
+
+export default Task
