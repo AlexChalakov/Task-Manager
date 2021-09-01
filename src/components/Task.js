aac3b5ea6cacb9c1edc21439a820b62d928682f2
Alexcheto <a.chalakov@lancaster.ac.uk>
2021-09-01 20:42:36 +0300

    deleteTask and reminder functions

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,9 @@
 import { FaTimesCircle } from 'react-icons/fa'
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete, onToggle }) => {
     return (
-        <div className = 'task'>
-            <h3> {task.text} <FaTimesCircle style={{ color: 'red', cursor: 'pointer' }} />  </h3>
+        <div className = 'task' onClick ={() => onToggle(task.id)}>
+            <h3> {task.text} <FaTimesCircle style={{ color: 'red', cursor: 'pointer' }} onClick = {() => onDelete(task.id)}/> </h3>
             <p> {task.day} </p>
         </div>
     )
